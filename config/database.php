3a14b6390b815f139b1497683a872f9cfeb423db
imkysou <1669808160@qq.com>
2024-08-15 20:53:19 +0800

    Upload test

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Change your database config here.
+ */
+$database = array(
+    # Database host, such as 127.0.0.1, localhost or sql.mydatabase.com
+    "host"=> "127.0.0.1",
+    # Database port, default is 3306
+    "port"=> 3306,
+    # Database username
+    "user"=> "root",
+    # Database password
+    "password"=> "root",
+    # Database DB name
+    "database"=> "tell",
+    # The pre of database
+    "pre"=> "tell",
+    # Data salt, change it before you install database!
+    "salt"=> "qwertyuiopasdfghjklzxcvbnm123"
+);
